50638f7bdd5299dd3d2415ab197db377f40f2a1e
Will Hodgson <will.hodgson@current-rms.com>
2019-12-02 16:29:53 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     respond_to do |format|
-      format.html
+      format.html { render :show }
     end
   end
 
